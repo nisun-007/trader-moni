dc89e3837e376b87b4884a1ae7af0bf5ca1ca30b
Adeniyi <adedotuniyi@gmail.com>
2019-04-09 21:38:29 +0100

    init

---
+++ src/app/pages/e-commerce/list/fruits-list.ts
@@ -0,0 +1,16 @@
+export const fruits: State[] = [
+ {Name: 'BENUE STATE',Percentage:30},
+ {Name: 'LAGOS STATE',Percentage:50},
+ {Name: 'TARABA STATE',Percentage:89},
+ {Name: 'OYO STATE',Percentage:29},
+ {Name: 'TARABA STATE',Percentage:19},
+ {Name: 'TARABA STATE',Percentage:79},
+ {Name: 'TARABA STATE',Percentage:69},
+ {Name: 'DELTA STATE',Percentage:39},
+ 
+];
+
+export class State{
+  Name: string;
+  Percentage: number;
+}
---
+++ src/app/pages/tables/tables.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { TablesRoutingModule, routedComponents } from './tables-routing.module';
+
+@NgModule({
+  exports: [...routedComponents],
+  imports: [
+    ThemeModule,
+    TablesRoutingModule,
+    Ng2SmartTableModule,
+  ],
+  declarations: [
+    ...routedComponents,
+  ],
+})
+export class TablesModule { }
